9aa243e259cecee322bf5b4c4ed9764e48ff24bb
Diego Sarmentero <diego.sarmentero@ninja-ide.org>
2012-07-21 18:44:34 -0300

    submitting files

---
+++ pyday.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 *-*
+import os
+
+from google.appengine.ext.webapp import template
+from google.appengine.api import users
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+
+class MainPage(webapp.RequestHandler):
+    def get(self):
+        path = os.path.join(os.path.dirname(__file__), "templates/index.html")
+        self.response.out.write(template.render(path, {}))
+
+
+class Register(webapp.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            path = os.path.join(os.path.dirname(__file__),
+                "templates/user/register.html")
+            self.response.out.write(template.render(path, {}))
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
+
+
+class About(webapp.RequestHandler):
+    def get(self):
+        path = os.path.join(os.path.dirname(__file__),
+            "templates/conference/about.html")
+        self.response.out.write(template.render(path, {}))
+
+
+def main():
+    application = webapp.WSGIApplication([
+        ('/', MainPage),
+        ('/register', Register),
+        ('/about', About),
+        ], debug=True)
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
---
+++ static/js/daysleft.js
@@ -0,0 +1,16 @@
+var today = new Date();
+var pyday = new Date(2012, 8, 15);
+var day = 24*60*60*1000;
+var daysLeft = Math.ceil((pyday.getTime() - today.getTime())/day);
+var wordLeft = "Faltan ";
+var wordDay = " días";
+
+if (daysLeft === 1) {
+	wordDay = " día";
+	wordLeft = "Falta ";
+}
+else if (daysLeft < 0) {
+	daysLeft = 0;
+}
+
+document.write(wordLeft + "<span>" + daysLeft + "</span>" + wordDay);
